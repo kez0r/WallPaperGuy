543cacd2a52f007c3a6e6a3e06ed57257c842709
Shawn <kez@jigmatic.com>
2016-12-19 20:37:17 -0500

    Initial Commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WallPaperGuy
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.groupBoxMain = new System.Windows.Forms.GroupBox();
+            this.pbPreview = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.wallpaperTimer = new System.Windows.Forms.Timer(this.components);
+            this.groupBoxMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxMain
+            // 
+            this.groupBoxMain.Controls.Add(this.pbPreview);
+            this.groupBoxMain.Controls.Add(this.label2);
+            this.groupBoxMain.Controls.Add(this.txtPath);
+            this.groupBoxMain.Controls.Add(this.label1);
+            this.groupBoxMain.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxMain.Location = new System.Drawing.Point(2, 3);
+            this.groupBoxMain.Name = "groupBoxMain";
+            this.groupBoxMain.Size = new System.Drawing.Size(277, 272);
+            this.groupBoxMain.TabIndex = 0;
+            this.groupBoxMain.TabStop = false;
+            this.groupBoxMain.Text = " Current Wallpaper Details... ";
+            // 
+            // pbPreview
+            // 
+            this.pbPreview.Location = new System.Drawing.Point(3, 114);
+            this.pbPreview.Name = "pbPreview";
+            this.pbPreview.Size = new System.Drawing.Size(270, 143);
+            this.pbPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPreview.TabIndex = 3;
+            this.pbPreview.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Preview:";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(3, 38);
+            this.txtPath.Multiline = true;
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtPath.Size = new System.Drawing.Size(270, 57);
+            this.txtPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Path:";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(204, 281);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(48, 281);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(150, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open Containing Folder...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // wallpaperTimer
+            // 
+            this.wallpaperTimer.Interval = 200;
+            this.wallpaperTimer.Tick += new System.EventHandler(this.wallpaperTimer_Tick);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 311);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.groupBoxMain);
+            this.Font = new System.Drawing.Font("Segoe UI Symbol", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.Text = "WallPaperGuy by Kez";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBoxMain.ResumeLayout(false);
+            this.groupBoxMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPreview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxMain;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.PictureBox pbPreview;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Timer wallpaperTimer;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Management;
+
+namespace WallPaperGuy
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        //retrieve the file path of the currently set wallpaper (only supports windows 7, 8, 10 & maybe XP & vista although it's untested)
+        internal static string GetWallpaperPath()
+        {
+            try
+            {
+                string wallpaperFile;
+
+                switch (GetWindowsVersionNumber())
+                {
+                    case null:
+                        return null;
+                    case "XP": //windows XP (untested)
+                        var xpReg = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Internet Explorer\\Desktop\\General\\", false);
+                        if (xpReg == null) return null;
+
+                        wallpaperFile = xpReg.GetValue("WallpaperSource").ToString();
+                        xpReg.Close();
+                        break;
+                    case "Vista": //windows vista (untested)
+                        var vistaReg = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Internet Explorer\\Desktop\\General\\", false);
+                        if (vistaReg == null) return null;
+
+                        wallpaperFile = vistaReg.GetValue("WallpaperSource").ToString();
+                        vistaReg.Close();
+                        break;
+                    case "7": //windows 7
+                        var w7Reg = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Internet Explorer\\Desktop\\General\\", false);
+                        if (w7Reg == null) return null;
+
+                        wallpaperFile = w7Reg.GetValue("WallpaperSource").ToString();
+                        w7Reg.Close();
+                        break;
+                    default: //all other windows, but assumed 8/10
+                        Console.WriteLine("8/10");
+                        var openSubKey = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop");
+                        if (openSubKey == null) return null;
+                        
+                        var path = (byte[])openSubKey.GetValue("TranscodedImageCache");
+                        wallpaperFile = Encoding.Unicode.GetString(Chop(path, 24)).TrimEnd("\0".ToCharArray());
+                        break;
+                }
+
+                return wallpaperFile;
+            }
+            catch (Exception) { return null; }
+        }
+
+        //return windows version (ie. XP, Vista, 7, 8, 10)
+        internal static string GetWindowsVersionNumber()
+        {
+            var osName = (from cap in new ManagementObjectSearcher("SELECT Caption FROM Win32_OperatingSystem").Get().Cast<ManagementObject>()
+                select cap.GetPropertyValue("Caption")).FirstOrDefault();
+
+            if (osName == null) return null;
+            var infoArray = osName.ToString().Split(' ');
+            
+            return infoArray[2];
+        }
+
+        static byte[] Chop(byte[] source, int pos)
+        {
+            var dest = new byte[source.Length - pos];
+            Array.Copy(source, pos, dest, 0, dest.Length);
+
+            return dest;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            wallpaperTimer.Enabled = true; //start wallpaper timer
+        }
+
+        private void wallpaperTimer_Tick(object sender, EventArgs e)
+        {
+            //get wallpaper path and filename
+            var path = GetWallpaperPath();
+
+            //check if wallpaper is removed (or set or solid color) during operation and reset controls
+            if (path == null || path.Trim() == "")
+            {
+                txtPath.Text = "";
+                pbPreview.Image = null;
+                return;
+            }
+
+            //set path text
+            txtPath.Text = path;
+
+            //load preview
+            if (txtPath.Text != "" && File.Exists(txtPath.Text))
+                pbPreview.Load(txtPath.Text);
+        }
+
+        //open containing folder of image
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var path = Path.GetDirectoryName(txtPath.Text);
+
+                Process.Start("explorer.exe", @path);
+            }
+            catch (Exception) { MessageBox.Show("Error: Path doesn't exist!", "WallPaperGuy by Kez", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+
+        //delete current wallpaper (might be quirky on windows 10)
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var fileName = Path.GetFileName(txtPath.Text);
+
+                if (fileName == null || fileName.Trim() == "") return;
+
+                var answer = MessageBox.Show("Are you sure you want to delete file: [" + fileName + "]?", "WallPaperGuy by Kez", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (answer != DialogResult.Yes) return;
+                
+                File.Delete(txtPath.Text);
+            }
+            catch (Exception) { MessageBox.Show("Error!", "WallPaperGuy by Kez", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+    }
+}
